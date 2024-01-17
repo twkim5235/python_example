03f38cd49c89a921c9b0f4e4f40d052ee710b498
twkim5235 <sssunggure@naver.com>
2024-01-17 19:13:57 +0900

    [INIT] python_example study start

---
+++ 2024/0116/square.py
@@ -0,0 +1,5 @@
+
+print('제곱 값 구하기')
+leg = int(input('num: '))
+
+print('result: ', leg * leg)
---
+++ 2024/0117/while2.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+i = 0
+
+while i < num:
+    print(num)
+    i += 1
+
+
