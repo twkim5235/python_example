5af281f524ac1e590eb8693e2233d1da96a0e035
Taewoo Kim <sssunggure@naver.com>
2024-01-24 20:38:32 +0900

    [ADD] 함수 연습1

---
+++ 2024/0124/function/function.py
@@ -0,0 +1,17 @@
+a_list = [3, 4, 62, 27, 83, 956, 26, 58, 3, 78, 168, 64, 78]
+
+
+def print_list(_list):
+    for i in _list:
+        print(i)
+
+
+print_list(a_list)
+
+
+def boy():
+    print('I am a boy')
+    print('You are a girl')
+
+
+boy()
